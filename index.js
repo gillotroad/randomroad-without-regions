e23d75d6bb930eb19b02053e5fa5eccabc9874ad
gillotroad <164035322+gillotroad@users.noreply.github.com>
2024-03-29 20:55:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,27 +12,26 @@ Manual:
 
 
 Version:
-28.03.24
-12:23
+29.03.24
+20:52
 
 */
 
-var startTime, path, panorama, startLoc, currentLatLong, tempControlUI;
+var startTime, path, panorama, startLoc, currentLatLong, tempControlUI, mapClickListener;
 let map, guessMarker, targetMarker, targetPath, replyText;
+
+const zeroPosition = { lat: 0, lng: 0 };
 
 
 async function initPano() {
   const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
   const { Geometry } = await google.maps.importLibrary("geometry");
 
-  const zeroPosition = { lat: 0, lng: 0 };
-
-  
+    
   newSpot();
   
-  //
-  
+  //Create map for guessing
   map = new Map(document.getElementById("map"), {
   	center: zeroPosition,
     zoom: 1,
@@ -41,15 +40,11 @@ async function initPano() {
     },
     disableDefaultUI: true,
 	mapId: "f0133ed91e943e1c",
+	draggableCursor: 'default',
   });
   
   
-  //
-  
-  // Note: constructed panorama objects have visible: true
-  // set by default.
-  
-  //
+  //Create StreetView panorama
   panorama = new google.maps.StreetViewPanorama(
     document.getElementById("pano"),
     {
@@ -61,9 +56,8 @@ async function initPano() {
       disableDefaultUI: false,
     },
   );
-  
-  
-  
+    
+    
   //Create "New Game" control button in top right corner of panorama
   var newGameControlDiv = document.createElement("div");
   tempControlUI = createControl(newGameControlDiv, "Starts a New Game Session", "New Game", "5px", "25px");
@@ -75,22 +69,30 @@ async function initPano() {
   //
   //Create "Submit" control button in top left corner of map
   var submitControlDiv = document.createElement("div");
-  tempControlUI = createControl(submitControlDiv, "Submit", "Submit", "3px", "12px");
+  tempControlUI = createControl(submitControlDiv, "Submit", "Submit", "3px", "16px");
   tempControlUI.addEventListener("click", () => {
   		submitGuess();
   });
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(submitControlDiv);
-  //
   
-  //
-  
+      
   map.setStreetView(panorama);
   
+  
+  //Create PinElement for guessMarker
+  const pinGuessMarker = new PinElement({
+  	scale: 0.7,
+	background: "#1A3CD5",
+	borderColor: "black",
+	glyphColor: "black",
+  });
+    
   //Create marker for guessing
   guessMarker = new AdvancedMarkerElement({
     map: map,
     position: { lat: 0, lng: 0 },
 	title: "My guess",
+	content: pinGuessMarker.element,
   });
   
   /*
@@ -102,19 +104,10 @@ async function initPano() {
   });
   */
   
-  map.addListener('click', function(event) {
+  mapClickListener = google.maps.event.addListener(map, 'click', function(event) {
   	moveMarker(event.latLng);
   });
   
-
-    
-  /*
-  currentLatLong = panorama.getPosition();
-  console.log("lat = ");
-  console.log(currentLatLong.lat);
-  console.log("long = ");
-  console.log(currentLatLong.long);
-  */
   
 }
 
@@ -123,15 +116,28 @@ async function initPano() {
 
 initPano();
 
-function newSpot() 
+async function newSpot() 
 {
-    startTime = new Date().getTime();
+	const { Map } = await google.maps.importLibrary("maps");
+	
+	startTime = new Date().getTime();
     try {
+		//Set click listener again for moving guessMarker
+		mapClickListener = google.maps.event.addListener(map, 'click', function(event) {
+  			moveMarker(event.latLng);
+  		});
+		
+		//center map at {lat: 0, lng: 0}
+		map.setCenter(zeroPosition);
+		
+		//reset guessMarker position to {lat: 0, lng: 0}
+		guessMarker.position = zeroPosition;
+		
+		//Reset targetMarker + targetPath, i.e. hide them from map
         targetMarker.setMap(null);
         targetMarker = null;
         targetPath.setMap(null);
-        targetPath = null;
-        
+        targetPath = null;		    
     }
     catch(err) {}
     finally {
@@ -170,6 +176,7 @@ function createControl(controlDiv, desc, content, bSize, fSize)
     controlUI.style.textAlign = "center";
     controlUI.title = desc;
     controlDiv.appendChild(controlUI);
+	
     // Set CSS for the control interior.
     const controlText = document.createElement("div");
     controlText.style.color = "rgb(25,25,25)";
@@ -190,21 +197,21 @@ function moveMarker(pnt)
 
 async function submitGuess()
 {
-	const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+	const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
 	
 	var gameDuration = formatTime((new Date().getTime() - startTime) / 1000);
-    //var latPan = panorama.getPosition().lat();
-    //var lngPan = panorama.getPosition().lng();
-    //var latGuessMarker = guessMarker.getPosition().lat();
-    //var lngGuessMarker = guessMarker.getPosition().lng();
 	var panPosition = panorama.getPosition();
 	var guessMarkerPosition = guessMarker.position;
 	
 	try {
-        targetMarker.setMap(null);
+		//Remove click listener so that guessMarker cannot be moved until new game starts
+        google.maps.event.clearListeners(map, 'click');
+		
+		//Reset targetMarker + targetPath
+		targetMarker.setMap(null);
         targetMarker = null;
         targetPath.setMap(null);
-        targetPath = null;
+        targetPath = null;			
     }
     catch (err) {
 	
@@ -224,20 +231,32 @@ async function submitGuess()
 		}
 		
 		replyText = '<div id="result">'+'<b>Result:</b><br>Distance: '  + distanceText + "  <br>" + 'Time: ' + gameDuration + "</div>";
-		console.log(replyText);
 		
+		
+		//Create PinElement for targetMarker
+  		const pinTargetMarker = new PinElement({
+			scale: 0.7,
+			background: "#1CEC12",
+			borderColor: "black",
+			glyphColor: "black",
+  		});
+		
+		//Create marker for showing target
 		targetMarker = new AdvancedMarkerElement ({
   			map: map,
 			position: panPosition,
 			draggable: false,
 			title: "Target",
+			content: pinTargetMarker.element,
   		});
 		
+		//Set path coordinates (guess -> target)
 		var pathCoordinates = [
             targetMarker.position,
             guessMarker.position
         ];
 		
+		//Create path
 		targetPath = new google.maps.Polyline({
         	path: pathCoordinates,
             geodesic: true,
@@ -248,9 +267,8 @@ async function submitGuess()
 		
 		targetPath.setMap(map);
 		
-		
+		//Show result popup
         displayPopup(replyText, map, targetMarker);
-		
 		
 	}
 }
